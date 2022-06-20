350789c04033fcde06f5e1f3df593fc5c3f182de
Nguyen Chi Nhan <519h0127@student.tdtu.edu.vn>
2022-06-20 14:22:38 +0700

    Create schemas and host dynamic json data.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const schema = require('./schemas/schema')
+const expressGraphQL = require('express-graphql').graphqlHTTP
+
+const app = express()
+
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql: true
+}))
+
+app.listen(4000, () => {
+    console.log(`Server is running on ${4000}`)
+})
